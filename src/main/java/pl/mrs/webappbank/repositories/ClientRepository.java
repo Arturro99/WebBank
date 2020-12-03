bb49a9e5c43a765d9ac9b0ff5fe09ca27b5dab6a
Artur <artur.m99@wp.pl>
2020-12-03 23:07:49 +0100

    Initialization is now taking place in ClientRepository

--- src/main/java/pl/mrs/webappbank/repositories/ClientRepository.java
+++ src/main/java/pl/mrs/webappbank/repositories/ClientRepository.java
@@ -1,7 +1,8 @@
 package pl.mrs.webappbank.repositories;
 
+import pl.mrs.webappbank.model.Currency;
+import pl.mrs.webappbank.model.accounts.*;
 import pl.mrs.webappbank.modelv2.Client;
-import pl.mrs.webappbank.model.accounts.Account;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,17 @@ public class ClientRepository implements IRepository<Client,String> {
 
     public ClientRepository() {
         this.clients = new ArrayList<>();
+        Client c1 = new Client("002020202020", "destroyer69", "1234", "dupa","blada",18);
+        Client c2 = new Client("481828218181", "qwerty", "567", "Ziomson","PL",12);
+        Client c3 = new Client("156549", "azerty", "666", "JP","dwa",8);
+        clients.add(c1);
+        clients.add(c2);
+        clients.add(c3);
+
+        c1.addAccount(new CommonAccount("123456789", 9990));
+        c2.addAccount(new CommonAccount("654312313", 90));
+        c2.addAccount(new CurrencyAccount("99876786", 94990, Currency.EUR));
+        c3.addAccount(new CommonAccount("6666669954", 850));
     }
 
     @Override
